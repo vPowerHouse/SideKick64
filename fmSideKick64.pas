6fe51f3ee7959b4e693ee21b4f6957d66dff9879
vPowerHouse <pfoley.vph@gmail.com>
2025-07-16 10:10:53 -0500

    Add files via upload

--- fmSideKick64.pas
+++ fmSideKick64.pas
@@ -5,47 +5,56 @@ unit fmSideKick64;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
-   System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
-    Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.Grids, Vcl.CheckLst, Vcl.Buttons, patTimerEX;
+  Winapi.Windows, Winapi.Messages, System.SysUtils,// System.Variants,
+   System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.Forms,// Vcl.Dialogs,
+    Vcl.Buttons, Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.Grids, Vcl.CheckLst;
 
 type
+
   TSideKick = class(TForm)
-    SG: TStringGrid;
-    Log: TMemo;
-    FlowPanel1: TFlowPanel;
+
     btnLoad: TButton;
     More: TButton;
-    ChkLB: TCheckListBox;
     Banner: TPanel;
     Less: TButton;
     Available: TButton;
     Run1: TButton;
     TrackAll: TButton;
+    SG: TStringGrid;
+    Log: TMemo;
+    FlowPanel1: TFlowPanel;
+
     Show_Browser: TButton;
-    SaveSettings: TButton;
-    RestoreSettings: TButton;
+    Save_Settings: TButton;
+    Restore_Settings: TButton;
     LogCSV: TMemo;
-//  private
-    procedure SaveSettingsClick(Sender: TObject);
+
+    procedure AvailableClick(Sender: TObject);
+    procedure btnLoadClick(Sender: TObject);
+
+    procedure Save_SettingsClick(Sender: TObject);
 //  protected
     procedure FormCloseQuery(Sender: TObject; var CanClose: Boolean);
+    procedure FormCreate(Sender: TObject);
 //  public  //damaged these somehow
-    procedure AvailableClick(Sender: TObject);
+
     procedure InitWinList(Sender: TObject; inScrackAll: Boolean);
-    procedure btnLoadClick(Sender: TObject);
     procedure MoreLessClick(Sender: TObject);
-    procedure RestoreSettingsClick(Sender: TObject);
+    procedure Restore_SettingsClick(Sender: TObject);
     procedure Run1Click(Sender: TObject);
 
     procedure Show_BrowserClick(Sender: TObject);
 //    public  //damaged these somehow fixed by renaming Load button to btnLoad was Load
 //  still damaged 20224 sept 16
+
+  private
+      ChkLB: TCheckListBox;
+
+   // var
   end;
 
 var
   SideKick: TSideKick;
-  patTimerEX1: TpatTimerEX;
 implementation
 
 {$R *.dfm}
@@ -53,29 +62,49 @@ implementation
 uses
   System.StrUtils,
   System.IOUtils,
+//  ganttt,
   AppList;
 //  Shellapi;
 var
   Exes: TptrWins;
 
+
 procedure TSideKick.AvailableClick(Sender: TObject);
 begin
-  with ChkLB do
-  begin
-    Visible := not Visible;
-    Left := Available.Left;
-    Top := Available.Top + Available.Height + 50{MN for titlebar height};
-  end;
+  ChkLB.Left := Available.Left;
+  ChkLB.Top := Available.Top + Available.Height + 50 { MN for titlebar height };
+  ChkLB.Visible := not ChkLb.Visible;
+end;
+
+procedure TSideKick.btnLoadClick(Sender: TObject);
+var
+  ListallWindows: Boolean;
+begin
+  ListallWindows := (Sender as Tcomponent).Tag = 69;
+  InitWinList(Sender, ListallWindows);
 end;
 
 procedure TSideKick.FormCloseQuery(Sender: TObject; var CanClose: Boolean);
 begin
   if assigned(Exes) then
     begin
-      Exes.StateChangeRequests([skSave, sKDataSave]); // Oops!   was skstop
+      Exes.StateChangeRequests([skDataSave]);// , sKDataSave]); // Oops!   was skstop
       Exes.Free;
     end;
   CanClose := True;
+end;
+
+procedure TSideKick.FormCreate(Sender: TObject);
+begin
+  ChkLB := TCheckListBox.Create(Self);
+  with ChkLB do begin
+      Left := 100;
+      Top := 50;
+      ItemHeight := 27;
+      TabOrder := 5;
+      ChkLB.Visible := False;
+      ChkLB.Parent := Self;
+    end;
 end;
 
 procedure TSideKick.InitWinList(Sender: TObject; inScrackAll: Boolean);
@@ -101,21 +130,13 @@ begin
   Less.Enabled := inScrackAll;
 end;
 
-procedure TSideKick.btnLoadClick(Sender: TObject);
-var
-  ListallWindows: Boolean;
-begin
-  ListallWindows := (Sender as Tcomponent).Tag = 69;
-  InitWinList(Sender, ListallWindows);
-end;
-
 procedure TSideKick.MoreLessClick(Sender: TObject);
 var
   localInstruction: TskState;
   bMore: Boolean;
 begin
   bMore := (Sender as tcontrol).Name = 'More';
-  If bMore
+  if bMore
   then
     localInstruction := skEverythingTrue
   else
@@ -126,7 +147,7 @@ begin
   Less.Enabled := bMore;
 end;
 
-procedure TSideKick.RestoreSettingsClick(Sender: TObject);
+procedure TSideKick.Restore_SettingsClick(Sender: TObject);
 begin
   Exes.StateChangeRequests([skViewsSettingsRestore]);
 end;
@@ -137,15 +158,15 @@ begin
     then Exes.StateChangeRequests([skRunStop]);
 end;
 
-procedure TSideKick.SaveSettingsClick(Sender: TObject);
+procedure TSideKick.Save_SettingsClick(Sender: TObject);
 begin
   if assigned(Exes) then
     Exes.StateChangeRequests([skViewsSettingsSave]);
-
 end;
 
 procedure TSideKick.Show_BrowserClick(Sender: TObject);
 begin
+//  form1.show;
   //form2.Show;
   Banner.Caption := 'Browser window coming soon!';
 end;
